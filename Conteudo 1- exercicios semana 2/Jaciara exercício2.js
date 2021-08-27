8260f9c8622cff1c88a0e1b39366a8e7ce56ce3f
Jaciara <jaciarafeio@gmail.com>
2021-08-27 14:06:06 -0300

    comitando exercício 2 da semana 2 Prof Keyla

---
+++ Conteudo 1- exercicios semana 2/Jaciara exercício2.js
@@ -0,0 +1,6 @@
+alert("Seja bem vindo ao meu site!")
+alert(87 + 96);
+alert(5 * 8);
+            
+var listadecompras = ["Requeijão, nutella, pão integral, biscoitos e frutas"]
+alert(listadecompras);
